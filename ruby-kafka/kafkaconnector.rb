70310e4856669b7709d95cd0c2e582cea339c314
Mohammed <regoo707@gmail.com>
2020-05-17 07:48:33 -0400

    change ruby connector

--- ruby-kafka/kafkaconnector.rb
+++ ruby-kafka/kafkaconnector.rb
@@ -4,19 +4,10 @@ kafka = Kafka.new(
  [ARGV[0]]
     )
 
-    # Consumers with the same group id will form a Consumer Group together.
 consumer = kafka.consumer(group_id: ARGV[1]
 )
-
-# It's possible to subscribe to multiple topics by calling `subscribe`
-# repeatedly.
 consumer.subscribe("message_logs")
-
-# Stop the consumer when the SIGTERM signal is sent to the process.
-# It's better to shut down gracefully than to kill the process.
 trap("TERM") { consumer.stop }
-
-# This will loop indefinitely, yielding each message in turn.
 consumer.each_message do |message|
   puts message.topic, message.partition
   puts message.offset, message.key, message.value
